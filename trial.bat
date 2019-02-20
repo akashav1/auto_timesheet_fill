27d43534f80857e2a222f5eaba361cd25a2c6164
akashav1 <akashpatel886@gmail.com>
2019-02-20 11:05:40 -0500

    asd

--- trial.bat
+++ trial.bat
@@ -1,2 +1,1 @@
-start cmd.exe /K git add "Hours.xlsx" ^&^& exit
-start cmd.exe /K git commit -m  "Files added" ^&^& exit
+start cmd.exe /K git add "Hours.xlsx" & git commit -m  "Files added" ^&^& exit
